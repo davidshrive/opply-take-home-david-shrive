4a0c06843fbeaabdf8711773e5291a1d46962ab8
David Shrive <contact@davidshrive.co.uk>
2025-06-10 15:22:51 +0100

    More normalisation + score calc to functions

--- script.py
+++ script.py
@@ -6,89 +6,54 @@ with open('input/processed_buyer_leads.json', 'r') as inputFile:
 	inputData = json.load(inputFile)
 
 ## Normalise 
+def find_value(company, possible_keys):
+	for key in possible_keys:
+		if key in company.keys():
+			return company[key]
+	return "unknown"
+
 companies = {}
 
 for inputCompany in inputData:
 	company = {}
-	keys = list(inputCompany.keys())
-
-	name = "unknown"
-	if "company_name" in keys:
-		name = inputCompany["company_name"]
-	elif "company" in keys:
-		name = inputCompany["company"]
-	elif "name" in keys:
-		name = inputCompany["name"]
-	company["name"] =  name
-
-	description = "unknown"
-	if "info" in keys:
-		description = inputCompany["info"]
-	elif "desc" in keys:
-		description = inputCompany["desc"]
-	elif "description" in keys:
-		description = inputCompany["description"]
-	company["description"] =  description
-
-	labels = "unknown"
-	if "labels" in keys:
-		labels = inputCompany["labels"]
-	elif "keywords" in keys:
-		labels = inputCompany["keywords"]
-	elif "tags" in keys:
-		labels = inputCompany["tags"]
-	labels = list(map(lambda label:label.strip(), labels.split(";")))
-	company["labels"] = labels
-
-	website = "unknown"
-	if "website" in keys:
-		website = inputCompany["website"]
-	elif "site" in keys:
-		website = inputCompany["site"]
-	elif "url" in keys:
-		website = inputCompany["url"]
-	company["website"] =  website
-
-	products = "unknown"
-	if "products" in keys:
-		products = inputCompany["products"]
-	elif "items" in keys:
-		products = inputCompany["items"]
-	elif "product_list" in keys:
-		products = inputCompany["product_list"]
-	company["products"] =  products
-
-	# Lots of duplication here, can I add function?
+	company['name'] 			= find_value(inputCompany, ['company_name', 'company', 'name'])
+	company['description'] 		= find_value(inputCompany, ['info', 'desc', 'description'])
+	company['labels'] 			= list(map(lambda label:label.strip(), find_value(inputCompany, ['labels', 'keywords', 'tags']).split(";")))
+	company['website'] 			= find_value(inputCompany, ['website', 'site', 'url'])
+	company['products'] 		= find_value(inputCompany, ['products', 'product_list', 'items'])
 
 	# Dedupe by company name, if multiple found labels & products are merged.
 	# Can cause issues with duplicate products
-	if name not in companies.keys():
-		companies[name] = company
+	if company['name'] not in companies.keys():
+		companies[company['name']] = company
 	else:
-		companies[name]["labels"].extend(labels)
-		companies[name]["products"].extend(products)
+		companies[company['name']]["labels"].extend(company["labels"])
+		companies[company['name']]["products"].extend(company["products"])
 
 ## Score
-for companyName in companies:
-
+def calc_score(company, labels):
 	score = 0
 	## Check labels, +10 for each
-	labels = ['corn-starch', 'corn starch']
 	for label in labels:
-		if label in companies[companyName]['labels']:
+		if label in company['labels']:
 			score += 10
 
 	## Check ingredients, +5 for each
-	for product in companies[companyName]['products']:
+	for product in company['products']:
 		if "ingredients" in product.keys():
 			for ingredient in product["ingredients"]:
 				if ingredient in label:
 					score += 5
-	companies[companyName]['score'] = score
+	return score
+
+# These are the values we are looking for to increase a companies score
+labels = ['corn-starch','corn starch']
+
+for companyName in companies:
+	companies[companyName]['score'] = calc_score(companies[companyName],labels)
 
 ## Export
 with open("output/export.csv", "w") as outputFile:
-
 	# Write headers
 	writer = csv.DictWriter(outputFile, companies["Company 1"].keys())
 	writer.writeheader()
